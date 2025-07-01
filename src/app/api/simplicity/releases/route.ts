8030e91f24c4e4d08c117b913a3f04830d310631
blazehp <blazehunterhp@gmail.com>
2025-07-01 19:23:13 -0400

    Added Simplicity update download routing and executable

---
+++ src/app/api/simplicity/releases/route.ts
@@ -0,0 +1,54 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { handle } from "hono/vercel";
+
+const UPDATE_SIG = process.env.UPDATE_SIG
+const UPDATE_NOTES = process.env.UPDATE_NOTES
+
+const app = new Hono().basePath("/api/simplicity");
+
+app.use(
+  "/*",
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:8000",
+      "http://localhost:5173",
+      "http://localhost:1420",
+      "https://localhost:1420",
+      "http://127.0.0.1:3000",
+    ],
+    allowHeaders: [
+      "Content-Type",
+      "Authorization",
+      "apikey",
+      "Access-Control-Allow-Origin",
+    ],
+    allowMethods: ["GET"],
+  })
+);
+
+// Fixed /info endpoint
+app.get("/releases/:platform/:version", async (c) => {
+  const { platform, version } = c.req.param();
+  const currentVersion = "0.0.1"
+
+  console.log({ platform, version })
+
+  if (currentVersion === version) {
+    console.log('No Update Available')
+    // No Update available
+    return c.status(204)
+  }
+
+  if (platform === 'windows') {
+    return c.json({
+    version: version,
+    url: `https://budgetary.codewithali.com/downloads/Simplicity_${version}_x64-setup.exe`,
+    signature: UPDATE_SIG,
+    notes: UPDATE_NOTES,
+  }, 200)
+  }
+});
+
+export const GET = handle(app);
